e26e831ca460827da019d594ede63998ca7b423a
Carlo Parata <carlo.parata@st.com>
2021-10-15 18:36:25 +0200

    Fix Astyle errors

--- src/rfal_rfst25r3911.h
+++ src/rfal_rfst25r3911.h
@@ -208,11 +208,12 @@ struct Callback;
 
 template <typename Ret, typename... Params>
 struct Callback<Ret(Params...)> {
-   template <typename... Args>
-   static Ret callback(Args... args) {
-      return func(args...);
-   }
-   static std::function<Ret(Params...)> func;
+  template <typename... Args>
+  static Ret callback(Args... args)
+  {
+    return func(args...);
+  }
+  static std::function<Ret(Params...)> func;
 };
 
 template <typename Ret, typename... Params>
